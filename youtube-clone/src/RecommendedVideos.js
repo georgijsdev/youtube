dc7204af5b9299ed9d362a573c4823b7db18762a
Georgijs Ventura <ventura55g@gmail.com>
2020-08-04 00:54:01 +0100

    first commit

---
+++ youtube-clone/src/RecommendedVideos.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import VideoCard from './VideoCard'
+import './RecommendedVideos.css'
+
+
+function RecommendedVideos() {
+    return (
+        <div className="recommendedVideos">
+            <h2>Recommended</h2>
+            <div className="recommendedVideos__videos">
+                <VideoCard 
+                    title="Become a Web Developer in 2020"
+                    views="1.4M Views"
+                    timestamp="4 days ago"
+                    channelImage=""
+                    channel="GeorgijsDev"
+                    image="https://www.greenmangaming.com/newsroom/wp-content/uploads/2017/08/youtube-logo1.jpg"
+                />
+                <VideoCard 
+                    title="Become a Web Developer in 2020"
+                    views="1.4M Views"
+                    timestamp="4 days ago"
+                    channelImage=""
+                    channel="GeorgijsDev"
+                    image="https://www.greenmangaming.com/newsroom/wp-content/uploads/2017/08/youtube-logo1.jpg"
+                />
+                <VideoCard 
+                    title="Become a Web Developer in 2020"
+                    views="1.4M Views"
+                    timestamp="4 days ago"
+                    channelImage=""
+                    channel="GeorgijsDev"
+                    image="https://www.greenmangaming.com/newsroom/wp-content/uploads/2017/08/youtube-logo1.jpg"
+                />
+                <VideoCard 
+                    title="Become a Web Developer in 2020"
+                    views="1.4M Views"
+                    timestamp="4 days ago"
+                    channelImage=""
+                    channel="GeorgijsDev"
+                    image="https://www.greenmangaming.com/newsroom/wp-content/uploads/2017/08/youtube-logo1.jpg"
+                />
+                <VideoCard 
+                    title="Become a Web Developer in 2020"
+                    views="1.4M Views"
+                    timestamp="4 days ago"
+                    channelImage=""
+                    channel="GeorgijsDev"
+                    image="https://www.greenmangaming.com/newsroom/wp-content/uploads/2017/08/youtube-logo1.jpg"
+                />
+                <VideoCard 
+                    title="Become a Web Developer in 2020"
+                    views="1.4M Views"
+                    timestamp="4 days ago"
+                    channelImage=""
+                    channel="GeorgijsDev"
+                    image="https://www.greenmangaming.com/newsroom/wp-content/uploads/2017/08/youtube-logo1.jpg"
+                />
+                <VideoCard 
+                    title="Become a Web Developer in 2020"
+                    views="1.4M Views"
+                    timestamp="4 days ago"
+                    channelImage=""
+                    channel="GeorgijsDev"
+                    image="https://www.greenmangaming.com/newsroom/wp-content/uploads/2017/08/youtube-logo1.jpg"
+                />
+            </div>
+        </div>
+    )
+}
+
+export default RecommendedVideos
+ 
---
+++ youtube-clone/src/SearchPage.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import ChannelRow from './ChannelRow'
+import VideoRow from './VideoRow'
+import TuneOutlinedIcon from '@material-ui/icons/TuneOutlined'
+import './SearchPage.css'
+
+function SearchPage() {
+    return (
+        <div className="searchPage">
+            <div className="searchPage__filter">
+                <TuneOutlinedIcon />
+                <h2>FILTER</h2>
+            </div>
+            <hr />
+
+            <ChannelRow 
+                image="https://cdn1.vectorstock.com/i/thumb-large/87/55/young-blond-smiling-man-face-avatar-positive-male-vector-17618755.jpg"
+                channel="GeorgijsDev"
+                verified
+                subs="209K"
+                noOfVideos={74}
+                description="Where you can find awesome programming tuts and development career advices"
+            />
+            <hr />
+
+            <VideoRow 
+                views="1.2M"
+                subs="209K"
+                description="Where you can find awesome programming tuts and development career advices"
+                timestamp="1 day ago"
+                channel="GeorgijsDev"
+                title="Let's build a portfolio"
+                image="https://www.greenmangaming.com/newsroom/wp-content/uploads/2017/08/youtube-logo1.jpg"
+            />
+            <VideoRow 
+                views="1.2M"
+                subs="209K"
+                description="Where you can find awesome programming tuts and development career advices"
+                timestamp="1 day ago"
+                channel="GeorgijsDev"
+                title="Let's build a portfolio"
+                image="https://www.greenmangaming.com/newsroom/wp-content/uploads/2017/08/youtube-logo1.jpg"
+            />
+            <VideoRow 
+                views="1.2M"
+                subs="209K"
+                description="Where you can find awesome programming tuts and development career advices"
+                timestamp="1 day ago"
+                channel="GeorgijsDev"
+                title="Let's build a portfolio"
+                image="https://www.greenmangaming.com/newsroom/wp-content/uploads/2017/08/youtube-logo1.jpg"
+            />
+        </div>
+    )
+}
+
+export default SearchPage
